e96786a2e7b15d82013f8b1ecf435486b00ef4b0
Kacper Paluch <70151618+kacpal@users.noreply.github.com>
2021-06-08 02:44:58 +0200

    Update setup.go

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,37 +1,42 @@
 package cmd
 
 import (
-	"flag"
-	"fmt"
-	"groestl/pkg/groestl"
-	"io/ioutil"
-	"os"
+        "flag"
+        "fmt"
+        "groestl/pkg/groestl"
+        "io/ioutil"
+        "os"
 )
 
+const Usage = `Usage:
+%s <args> <path>
+`
+
 func Execute() {
-	var sum []byte
+        var sum []byte
 
-	hashlen := flag.Int("hash", 256, "output hash length")
-	flag.Parse()
+        hashlen := flag.Int("hash", 256, "output hash length")
+        flag.Parse()
 
-	data, err := ioutil.ReadFile(flag.Arg(0))
-	if err != nil {
-		panic(err)
-	}
+        if flag.Arg(0) == "" {
+                fmt.Printf(Usage, os.Args[0])
+                os.Exit(1)
+        }
 
-	switch *hashlen {
-	case 224:
-		sum = groestl.Sum224(data)
-	case 256:
-		sum = groestl.Sum256(data)
-	case 384:
-		sum = groestl.Sum384(data)
-	case 512:
-		sum = groestl.Sum512(data)
-	default:
-		fmt.Println("Invalid hash length")
-		os.Exit(1)
-	}
+        data, err := ioutil.ReadFile(flag.Arg(0))
+        if err != nil {
+                panic(err)
+        }
 
-	fmt.Println(sum)
+        switch *hashlen {
+        case 256:
+                sum = groestl.Sum256(data)
+        case 512:
+                sum = groestl.Sum512(data)
+        default:
+                fmt.Println("Invalid hash length")
+                os.Exit(1)
+        }
+
+        groestl.PrintHash(sum)
 }

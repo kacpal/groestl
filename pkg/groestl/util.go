bebbade3bb03bee1f60192d66d9e952101f84759
Kacper Paluch <70151618+kacpal@users.noreply.github.com>
2021-06-08 12:49:20 +0200

    Documentation (#5)
    * Add groestl.go documentation
    
    * Add round.go documentation
    
    * Add util.go documentation

--- pkg/groestl/util.go
+++ pkg/groestl/util.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 )
 
+// Function for printing hash output from groestl.Sum()
 func PrintHash(hash []byte) {
 	fmt.Println(hex.EncodeToString(hash))
 }
 
+// Helper function for returning i'th row from provided
+// uint64
 func pickRow(col uint64, i int) byte {
 	return byte((col >> (8 * (7 - i))) & 0xFF)
 }
 
+// Prints provided x slice in form of block.
 func printUintSlice(x []uint64) {
 	l := len(x)
 	for i := 0; i < 8; i++ {
